d5ba0ba6d475669576af8f5e438e0828a7d59801
Mwaldemar <304837@via.dk>
2025-02-11 17:29:19 +0100

    Started working on the projects page. Almost done

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { ProjectPreview } from '@/components/ProjectPreview';
 import { getProjectMetadata } from '@/components/getProjectMetadata';
+import styles from './projects.module.scss'
 
 const Projects = () => {
-    const postMetadata = getProjectMetadata();
-    const postPreviews = postMetadata.map((post) => (
-        <ProjectPreview key={post.slug} {...post} />
+    const projectMetadata = getProjectMetadata();
+    const projectPreviews = projectMetadata.map((project) => (
+        <ProjectPreview key={project.slug} {...project} />
     ));
 
-    return <div>{postPreviews}</div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.projectContainer}>
+                <div className={styles.projectPreview}>
+                    {projectPreviews}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
